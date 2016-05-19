af2db37ffdfa08df8ec04403a16a2f10c53f614c
Renan Rolo <renanrolo@hotmail.com>
2016-05-19 12:21:45 -0400

    Comparações entre datas

--- PeriodosCsharp/PeriodosCsharp/Periodos.cs
+++ PeriodosCsharp/PeriodosCsharp/Periodos.cs
@@ -10,6 +10,124 @@ namespace PeriodosCsharp
 {
     public static class Periodos
     {
+        /// <summary>
+        /// Retorna o ultimo dia do mes da data de referencia.
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public static DateTime UltimoDiaDoMes(this DateTime data)
+        {
+            DateTime dataUltimoDiaDoMes = new DateTime(data.Year, data.Month, DateTime.DaysInMonth(data.Year, data.Month));
+            return dataUltimoDiaDoMes;
+        }
+
+        /// <summary>
+        /// Retorna o ultimo dia do mes da data de referencia.
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public static DateTime? UltimoDiaDoMes(this DateTime? data)
+        {
+            if (!data.HasValue)
+                return null;
+
+            DateTime dataUltimoDiaDoMes = new DateTime(data.Value.Year, data.Value.Month, DateTime.DaysInMonth(data.Value.Year, data.Value.Month));
+            
+            return dataUltimoDiaDoMes;
+        }
+
+        /// <summary>
+        /// Compara datas e retorna a maior. Permite datas nulas, mas retorna somente a maior data que tiver valor.
+        /// </summary>
+        /// <param name="datas">Datas para comparar</param>
+        /// <returns>Retorna a maior data, ou numa lista de nulos retorna null</returns>
+        public static DateTime? RetornaMaiorData(params DateTime?[] datas)
+        {
+            DateTime? retorno = null;
+
+            foreach (var item in datas)
+            {
+                if (!retorno.HasValue)
+                {
+                    retorno = item;
+                    continue;
+                }
+
+                if (item.HasValue && item.Value > retorno.Value)
+                {
+                    retorno = item;
+                }
+            }
+
+            return retorno;
+        }
+
+        /// <summary>
+        /// Compara datas e retorna a maior.
+        /// </summary>
+        /// <param name="datas">Datas para comparar</param>
+        /// <returns>Retorna a maior data</returns>
+        public static DateTime RetornaMaiorData(params DateTime[] datas)
+        {
+            DateTime retorno = new DateTime();
+
+            foreach (var item in datas)
+            {
+                if (item > retorno)
+                {
+                    retorno = item;
+                }
+            }
+
+            return retorno;
+        }
+
+        /// <summary>
+        /// Compara datas e retorna a menor. Permite datas nulas, mas retorna somente a menor data que tiver valor.
+        /// </summary>
+        /// <param name="datas">Datas para comparar</param>
+        /// <returns>Retorna a menor data, ou numa lista de nulos retorna null</returns>
+        public static DateTime? RetornaMenorData(params DateTime?[] datas)
+        {
+            DateTime? retorno = null;
+
+            foreach (var item in datas)
+            {
+                if (!retorno.HasValue)
+                {
+                    retorno = item;
+                    continue;
+                }
+
+                if (item.HasValue && item.Value < retorno.Value)
+                {
+                    retorno = item;
+                }
+            }
+
+            return retorno;
+        }
+
+        /// <summary>
+        /// Compara datas e retorna a menor.
+        /// </summary>
+        /// <param name="datas">Datas para comparar</param>
+        /// <returns>Retorna a menor data.</returns>
+        public static DateTime RetornaMenorData(params DateTime[] datas)
+        {
+            DateTime retorno = new DateTime(9999, 12, 31);
+
+            foreach (var item in datas)
+            {
+                if (item < retorno)
+                {
+                    retorno = item;
+                }
+            }
+
+            return retorno;
+        }
+
         public static Boolean VerificaPeriodoUnico(List<KeyValuePair<DateTime, DateTime?>> lista)
         {
             DateTime? dataInicioNaoUsada;
@@ -30,10 +148,8 @@ namespace PeriodosCsharp
                 if (lista == null)
                     return true;
 
-
                 if (lista.Where(x => x.Value.HasValue && x.Value.Value < x.Key).Any())
-                    throw new Exception("Foi encontrado um (ou mais) periodo(s) com data final menor que data inicial.");
-
+                    throw new Exception("VerificaPeriodoUnico - Foi encontrado um (ou mais) periodo(s) com data final menor que data inicial.");
 
                 if (lista.Count() <= 1)
                 {
@@ -45,7 +161,6 @@ namespace PeriodosCsharp
 
                     return true;
                 }
-
 
                 DateTime menorData = lista.Min(x => x.Key);
                 DateTime? maiorData = lista.Max(x => x.Value);
@@ -82,40 +197,196 @@ namespace PeriodosCsharp
             }
             catch (Exception ex)
             {
-                for (int i = 0; i < lista.Count; i++)
-                {
-                    ex.Data.Add("Lista[" + i + "]", "Ini: " + lista[i].Key + " => Fim: " + lista[i].Value);
-                }
-
                 throw new Exception(ex.Message, ex);
             }
         }
 
-        public static Boolean VerificaPeriodoUnico<TProperty>(this List<TProperty> lista, Expression<Func<TProperty, DateTime>> expressaoDtIni, Expression<Func<TProperty, DateTime?>> expressaoDtFim)
+        public static Boolean VerificaPeriodoUnico<TProperty>(this IEnumerable<TProperty> lista, Expression<Func<TProperty, DateTime>> expressaoDtIni, Expression<Func<TProperty, DateTime?>> expressaoDtFim)
         {
-            List<KeyValuePair<DateTime, DateTime?>> listaKey = new List<KeyValuePair<DateTime, DateTime?>>();
-
             var nomePropriedadeDtIni = (expressaoDtIni.Body as MemberExpression).Member.Name.ToString();
             var nomePropriedadeDFimi = (expressaoDtFim.Body as MemberExpression).Member.Name.ToString();
 
-            foreach (var item in lista)
-            {
-                object dtIniObject = item.GetType().GetProperty(nomePropriedadeDtIni).GetValue(item, null);
-                object dtFimObject = item.GetType().GetProperty(nomePropriedadeDFimi).GetValue(item, null);
-
-                DateTime dtInicioAux = (DateTime)dtIniObject;
-                DateTime? dtFimAux = dtFimObject as DateTime?;
-
-                var novoPeriodo = new KeyValuePair<DateTime, DateTime?>(dtInicioAux, dtFimAux);
-
-                listaKey.Add(novoPeriodo);
-            }
+            List<KeyValuePair<DateTime, DateTime?>> listaKey =
+                lista
+                .Select(x =>
+                    new KeyValuePair<DateTime, DateTime?>(
+                        (DateTime)x.GetType().GetProperty(nomePropriedadeDtIni).GetValue(x, null)
+                        , (DateTime?)x.GetType().GetProperty(nomePropriedadeDFimi).GetValue(x, null)))
+                .ToList();
 
             Boolean retorno = VerificaPeriodoUnico(listaKey);
 
             return retorno;
         }
 
+        public static Boolean VerificaPeriodoUnico<TProperty>(this IEnumerable<TProperty> lista, Expression<Func<TProperty, DateTime>> expressaoDtIni, Expression<Func<TProperty, DateTime?>> expressaoDtFim, out DateTime? menorDtIni, out DateTime? maiorDtFim)
+        {
+            var nomePropriedadeDtIni = (expressaoDtIni.Body as MemberExpression).Member.Name.ToString();
+            var nomePropriedadeDFimi = (expressaoDtFim.Body as MemberExpression).Member.Name.ToString();
+
+            List<KeyValuePair<DateTime, DateTime?>> listaKey =
+                lista
+                .Select(x =>
+                    new KeyValuePair<DateTime, DateTime?>(
+                        (DateTime)x.GetType().GetProperty(nomePropriedadeDtIni).GetValue(x, null)
+                        , (DateTime?)x.GetType().GetProperty(nomePropriedadeDFimi).GetValue(x, null)))
+                .ToList();
+
+            Boolean retorno = VerificaPeriodoUnico(listaKey, out menorDtIni, out maiorDtFim);
+
+            return retorno;
+        }
+
+        public static Boolean VerificaPeriodosSemConflito(List<KeyValuePair<DateTime, DateTime?>> lista)
+        {
+            if (lista == null || lista.Count() <= 1)
+            {
+                return true;
+            }
+
+            foreach (var item in lista)
+            {
+                var listaSemOProprioElemento = lista.Where(x => x.Key != item.Key).ToList();
+
+                if (item.Value.HasValue)
+                {
+                    if (listaSemOProprioElemento.Any(x => x.Value.HasValue && item.Key <= x.Value.Value && item.Value.Value >= x.Key))
+                    {
+                        return false;
+                    }
+                    if (listaSemOProprioElemento.Any(x => !x.Value.HasValue && item.Value.Value >= x.Key))
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    if (listaSemOProprioElemento.Any(x => !x.Value.HasValue))
+                    {
+                        return false;
+                    }
+                    if (listaSemOProprioElemento.Any(x => x.Value.HasValue && item.Key <= x.Value.Value))
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        public static Boolean VerificaPeriodosSemConflito<TProperty>(this IEnumerable<TProperty> lista, Expression<Func<TProperty, DateTime>> expressaoDtIni, Expression<Func<TProperty, DateTime?>> expressaoDtFim)
+        {
+            var nomePropriedadeDtIni = RetornaNomePropriedade(expressaoDtIni);
+            var nomePropriedadeDFimi = RetornaNomePropriedade(expressaoDtFim); ;
+
+            List<KeyValuePair<DateTime, DateTime?>> listaKey =
+                lista
+                .Select(x =>
+                    new KeyValuePair<DateTime, DateTime?>(
+                        (DateTime)x.GetType().GetProperty(nomePropriedadeDtIni).GetValue(x, null)
+                        , (DateTime?)x.GetType().GetProperty(nomePropriedadeDFimi).GetValue(x, null)))
+                .ToList();
+
+            Boolean retorno = VerificaPeriodosSemConflito(listaKey);
+
+            return retorno;
+        }
+
+        private static string RetornaNomePropriedade(LambdaExpression memberSelector)
+        {
+            if ((memberSelector.Body as MemberExpression).Expression.NodeType == ExpressionType.MemberAccess)
+            {
+                return ((memberSelector.Body as MemberExpression).Expression as MemberExpression).Member.Name;
+            }
+
+            return (memberSelector.Body as MemberExpression).Member.Name;
+        }
+
+        /// <summary>
+        /// Compara datas. 
+        /// </summary>
+        /// <param name="essaData"></param>
+        /// <param name="outraData">Data a ser comparada.</param>
+        public static Boolean MaiorQue(this DateTime essaData, DateTime outraData)
+        {
+            Boolean comparacao = false;
+
+            if (DateTime.Compare(essaData, outraData) > 0)
+            {
+                comparacao = true;
+            }
+
+            return comparacao;
+        }
+
+        /// <summary>
+        /// Compara datas. 
+        /// </summary>
+        /// <param name="essaData"></param>
+        /// <param name="outraData">Data a ser comparada.</param>
+        public static Boolean MaiorOuIgualQue(this DateTime essaData, DateTime outraData)
+        {
+            Boolean comparacao = false;
+
+            if (DateTime.Compare(essaData, outraData) > -1)
+            {
+                comparacao = true;
+            }
+
+            return comparacao;
+        }
+
+        /// <summary>
+        /// Compara datas. 
+        /// </summary>
+        /// <param name="essaData"></param>
+        /// <param name="outraData">Data a ser comparada.</param>
+        public static Boolean IgualQue(this DateTime essaData, DateTime outraData)
+        {
+            Boolean comparacao = false;
+
+            if (DateTime.Compare(essaData, outraData) == 0)
+            {
+                comparacao = true;
+            }
+
+            return comparacao;
+        }
+
+        /// <summary>
+        /// Compara datas. 
+        /// </summary>
+        /// <param name="essaData"></param>
+        /// <param name="outraData">Data a ser comparada.</param>
+        public static Boolean MenorQue(this DateTime essaData, DateTime outraData)
+        {
+            Boolean comparacao = false;
+
+            if (DateTime.Compare(essaData, outraData) == -1)
+            {
+                comparacao = true;
+            }
+
+            return comparacao;
+        }
+
+        /// <summary>
+        /// Compara datas. 
+        /// </summary>
+        /// <param name="essaData"></param>
+        /// <param name="outraData">Data a ser comparada.</param>
+        public static Boolean MenorOuIgualQue(this DateTime essaData, DateTime outraData)
+        {
+            Boolean comparacao = false;
+
+            if (DateTime.Compare(essaData, outraData) <= 0)
+            {
+                comparacao = true;
+            }
+
+            return comparacao;
+        }
 
     }
 }
---
+++ PeriodosCsharp/Testes/TestCompararDatas.cs
@@ -0,0 +1,334 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PeriodosCsharp;
+namespace Testes
+{
+    [TestClass]
+    public class TestCompararDatas
+    {
+        [TestMethod]
+        public void Testar_data_MariorQue_sendo_maior()
+        {
+            DateTime dataUm = new DateTime(2015, 10, 01);
+
+            DateTime dataDois = new DateTime(2015, 9, 01);
+
+            Boolean retorno = dataUm.MaiorQue(dataDois);
+
+            Assert.AreEqual(true, retorno);
+        }
+
+        [TestMethod]
+        public void Testar_data_MariorQue_sendo_igual()
+        {
+            DateTime dataUm = new DateTime(2015, 10, 01);
+
+            DateTime dataDois = new DateTime(2015, 10, 01);
+
+            Boolean retorno = dataUm.MaiorQue(dataDois);
+
+            Assert.AreEqual(false, retorno);
+        }
+
+        [TestMethod]
+        public void Testar_data_MariorQue_sendo_menor()
+        {
+            DateTime dataUm = new DateTime(2015, 10, 01);
+
+            DateTime dataDois = new DateTime(2015, 10, 02);
+
+            Boolean retorno = dataUm.MaiorQue(dataDois);
+
+            Assert.AreEqual(false, retorno);
+        }
+
+        [TestMethod]
+        public void Testar_data_MariorQue_sendo_maior_em_segundos()
+        {
+            DateTime dataUm = new DateTime(2015, 10, 01, 6, 15, 10);
+
+            DateTime dataDois = new DateTime(2015, 10, 01, 6, 15, 09);
+
+            Boolean retorno = dataUm.MaiorQue(dataDois);
+
+            Assert.AreEqual(true, retorno);
+        }
+
+        [TestMethod]
+        public void Testar_data_MariorQue_sendo_menor_em_segundos()
+        {
+            DateTime dataUm = new DateTime(2015, 10, 01, 6, 15, 10);
+
+            DateTime dataDois = new DateTime(2015, 10, 01, 6, 15, 11);
+
+            Boolean retorno = dataUm.MaiorQue(dataDois);
+
+            Assert.AreEqual(false, retorno);
+        }
+
+        [TestMethod]
+        public void Testar_data_MariorOuIgualQue_sendo_maior()
+        {
+            DateTime dataUm = new DateTime(2015, 10, 02);
+
+            DateTime dataDois = new DateTime(2015, 10, 01);
+
+            Boolean retorno = dataUm.MaiorOuIgualQue(dataDois);
+
+            Assert.AreEqual(true, retorno);
+        }
+
+        [TestMethod]
+        public void Testar_data_MariorOuIgualQue_sendo_menor()
+        {
+            DateTime dataUm = new DateTime(2015, 10, 01);
+
+            DateTime dataDois = new DateTime(2015, 10, 02);
+
+            Boolean retorno = dataUm.MaiorOuIgualQue(dataDois);
+
+            Assert.AreEqual(false, retorno);
+        }
+
+        [TestMethod]
+        public void Testar_data_MariorOuIgualQue_sendo_igual()
+        {
+            DateTime dataUm = new DateTime(2015, 10, 01);
+
+            DateTime dataDois = new DateTime(2015, 10, 01);
+
+            Boolean retorno = dataUm.MaiorOuIgualQue(dataDois);
+
+            Assert.AreEqual(true, retorno);
+        }
+
+        [TestMethod]
+        public void Testar_data_MariorOuIgualQue_sendo_maior_em_segundos()
+        {
+            DateTime dataUm = new DateTime(2015, 10, 01, 12, 30, 31);
+
+            DateTime dataDois = new DateTime(2015, 10, 01, 12, 30, 30);
+
+            Boolean retorno = dataUm.MaiorOuIgualQue(dataDois);
+
+            Assert.AreEqual(true, retorno);
+        }
+
+        [TestMethod]
+        public void Testar_data_MariorOuIgualQue_sendo_menor_em_segundos()
+        {
+            DateTime dataUm = new DateTime(2015, 10, 01, 12, 30, 30);
+
+            DateTime dataDois = new DateTime(2015, 10, 01, 12, 30, 31);
+
+            Boolean retorno = dataUm.MaiorOuIgualQue(dataDois);
+
+            Assert.AreEqual(false, retorno);
+        }
+
+        [TestMethod]
+        public void Testar_data_MariorOuIgualQue_sendo_iguais_em_segundos()
+        {
+            DateTime dataUm = new DateTime(2015, 10, 01, 0, 0, 0);
+
+            DateTime dataDois = new DateTime(2015, 10, 01);
+
+            Boolean retorno = dataUm.MaiorOuIgualQue(dataDois);
+
+            Assert.AreEqual(true, retorno);
+        }
+
+        [TestMethod]
+        public void Testar_data_IgualQue_sendo_iguais()
+        {
+            DateTime dataUm = new DateTime(2015, 10, 01, 0, 0, 0);
+
+            DateTime dataDois = new DateTime(2015, 10, 01);
+
+            Boolean retorno = dataUm.IgualQue(dataDois);
+
+            Assert.AreEqual(true, retorno);
+        }
+
+        [TestMethod]
+        public void Testar_data_IgualQue_sendo_iguais_em_segundos()
+        {
+            DateTime dataUm = new DateTime(2015, 10, 01, 0, 0, 0);
+
+            DateTime dataDois = new DateTime(2015, 10, 01);
+
+            Boolean retorno = dataUm.IgualQue(dataDois);
+
+            Assert.AreEqual(true, retorno);
+        }
+
+        [TestMethod]
+        public void Testar_data_IgualQue_sendo_maior()
+        {
+            DateTime dataUm = new DateTime(2015, 10, 02);
+
+            DateTime dataDois = new DateTime(2015, 10, 01);
+
+            Boolean retorno = dataUm.IgualQue(dataDois);
+
+            Assert.AreEqual(false, retorno);
+        }
+
+        [TestMethod]
+        public void Testar_data_IgualQue_sendo_maior_em_segundos()
+        {
+            DateTime dataUm = new DateTime(2015, 10, 01, 0, 0, 30);
+
+            DateTime dataDois = new DateTime(2015, 10, 01);
+
+            Boolean retorno = dataUm.IgualQue(dataDois);
+
+            Assert.AreEqual(false, retorno);
+        }
+
+        [TestMethod]
+        public void Testar_data_IgualQue_sendo_menor()
+        {
+            DateTime dataUm = new DateTime(2015, 10, 01);
+
+            DateTime dataDois = new DateTime(2015, 10, 02);
+
+            Boolean retorno = dataUm.IgualQue(dataDois);
+
+            Assert.AreEqual(false, retorno);
+        }
+
+        [TestMethod]
+        public void Testar_data_IgualQue_sendo_menor_em_segundos()
+        {
+            DateTime dataUm = new DateTime(2015, 10, 01, 0, 0, 30);
+
+            DateTime dataDois = new DateTime(2015, 10, 01, 0, 0, 45);
+
+            Boolean retorno = dataUm.IgualQue(dataDois);
+
+            Assert.AreEqual(false, retorno);
+        }
+
+        [TestMethod]
+        public void Testar_data_MenorQue_sendo_menor()
+        {
+            DateTime dataUm = new DateTime(2015, 10, 01);
+
+            DateTime dataDois = new DateTime(2015, 10, 02);
+
+            Boolean retorno = dataUm.MenorQue(dataDois);
+
+            Assert.AreEqual(true, retorno);
+        }
+
+        [TestMethod]
+        public void Testar_data_MenorQue_sendo_menor_em_segundos()
+        {
+            DateTime dataUm = new DateTime(2015, 10, 01, 0, 0, 30);
+
+            DateTime dataDois = new DateTime(2015, 10, 01, 0, 0, 45);
+
+            Boolean retorno = dataUm.MenorQue(dataDois);
+
+            Assert.AreEqual(true, retorno);
+        }
+
+        [TestMethod]
+        public void Testar_data_MenorQue_sendo_maior()
+        {
+            DateTime dataUm = new DateTime(2015, 10, 02);
+
+            DateTime dataDois = new DateTime(2015, 10, 01);
+
+            Boolean retorno = dataUm.MenorQue(dataDois);
+
+            Assert.AreEqual(false, retorno);
+        }
+
+        [TestMethod]
+        public void Testar_data_MenorQue_sendo_maior_em_segundos()
+        {
+            DateTime dataUm = new DateTime(2015, 10, 01, 0, 0, 45);
+
+            DateTime dataDois = new DateTime(2015, 10, 01, 0, 0, 15);
+
+            Boolean retorno = dataUm.MenorQue(dataDois);
+
+            Assert.AreEqual(false, retorno);
+        }
+
+
+        [TestMethod]
+        public void Testar_data_MenorOuIgualQue_sendo_maior()
+        {
+            DateTime dataUm = new DateTime(2015, 10, 02);
+
+            DateTime dataDois = new DateTime(2015, 10, 01);
+
+            Boolean retorno = dataUm.MenorOuIgualQue(dataDois);
+
+            Assert.AreEqual(false, retorno);
+        }
+
+        [TestMethod]
+        public void Testar_data_MenorOuIgualQue_sendo_maior_em_segundos()
+        {
+            DateTime dataUm = new DateTime(2015, 10, 01, 0, 0, 45);
+
+            DateTime dataDois = new DateTime(2015, 10, 01, 0, 0, 15);
+
+            Boolean retorno = dataUm.MenorOuIgualQue(dataDois);
+
+            Assert.AreEqual(false, retorno);
+        }
+
+        [TestMethod]
+        public void Testar_data_MenorOuIgualQue_sendo_igual()
+        {
+            DateTime dataUm = new DateTime(2015, 10, 01);
+
+            DateTime dataDois = new DateTime(2015, 10, 01);
+
+            Boolean retorno = dataUm.MenorOuIgualQue(dataDois);
+
+            Assert.AreEqual(true, retorno);
+        }
+
+        [TestMethod]
+        public void Testar_data_MenorOuIgualQue_sendo_igual_em_segundos()
+        {
+            DateTime dataUm = new DateTime(2015, 10, 01, 12, 30, 15);
+
+            DateTime dataDois = new DateTime(2015, 10, 01, 12, 30, 15);
+
+            Boolean retorno = dataUm.MenorOuIgualQue(dataDois);
+
+            Assert.AreEqual(true, retorno);
+        }
+
+        [TestMethod]
+        public void Testar_data_MenorOuIgualQue_sendo_menor()
+        {
+            DateTime dataUm = new DateTime(2015, 10, 01);
+
+            DateTime dataDois = new DateTime(2015, 10, 02);
+
+            Boolean retorno = dataUm.MenorOuIgualQue(dataDois);
+
+            Assert.AreEqual(true, retorno);
+        }
+
+        [TestMethod]
+        public void Testar_data_MenorOuIgualQue_sendo_menor_em_segundos()
+        {
+            DateTime dataUm = new DateTime(2015, 10, 01, 12, 30, 00);
+
+            DateTime dataDois = new DateTime(2015, 10, 01, 12, 30, 15);
+
+            Boolean retorno = dataUm.MenorOuIgualQue(dataDois);
+
+            Assert.AreEqual(true, retorno);
+        }
+    }
+}
